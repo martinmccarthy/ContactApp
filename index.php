105ba12947cf356ac1fa1b7479d86e883e703fdf
martin <martinmccarthy@knights.ucf.edu>
2021-05-19 17:07:58 -0400

    creates a header for the index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Con-Tacks</title>
+        <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+        <?php 
+            include 'footer.php';
+        ?>
+    </body>
+</html>
