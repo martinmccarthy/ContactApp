05e8bf9f24c70c6aacef917459672e66f0756302
martin <martinmccarthy@knights.ucf.edu>
2021-05-20 16:27:52 -0400

    adds search bar

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Con-Tacks</title>
-        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" href="css/main.css">
     </head>
     <body>
         <?php 
