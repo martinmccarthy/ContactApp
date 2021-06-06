736829fe67b99fb2ec71181ac67c813dc63fa37e
SSingh1000 <84424375+SSingh1000@users.noreply.github.com>
2021-06-06 12:18:02 -0400

    Update DeleteContact.php

--- API/DeleteContact.php
+++ API/DeleteContact.php
@@ -3,48 +3,56 @@
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
-	
-	$inData = getRequestInfo();
-	
-	
 
-	$Login = $inData["Login"];
+$inData = getRequestInfo();
+
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	$email = $inData["email"];
 	$phone = $inData["phone"];
-
-
-
+	$Login = $inData["Login"];
 	
 
 	$conn = new mysqli("localhost", "web", "Group232021Summ3r", "Contacks");
-	if ($conn->connect_error)
-	{
+	if ($conn->connect_error){
+
 		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-		$stmt = $conn->prepare("DELETE FROM Contacts Where (Login = ? or phone = ?) ");
-		$stmt->bind_param("ss", $Login, $phone);
-		$results = $stmt->execute();
-		returnWithError($stmt->error);
+
+	}else{
+
+		$stmt = $conn->prepare("DELETE FROM Contacts WHERE (FirstName = ? AND LastName = ? AND email = ? AND phone = ? AND Login = ?)");
+		$stmt->bind_param("sssss", $FirstName, $LastName, $email, $phone, $Login);
+		$stmt->execute();
 		$stmt->close();
 		$conn->close();
-
+		
+		if( $stmt->error ){
+			returnWithError($stmt->error);
+		}else{
+			returnWithSuccess();
+		}
 	}
 
-	function getRequestInfo()
-	{
+	function getRequestInfo(){
+
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
-	function sendResultInfoAsJson( $obj )
-	{
+	function sendResultInfoAsJson( $obj ){
+
 		header('Content-type: application/json');
 		echo $obj;
 	}
 
-	function returnWithError( $err )
-	{
+	function returnWithError( $err ){
+
 		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	function returnWithSuccess(){
+
+		$retValue = '{"success": "Contact Deleted"}';
 		sendResultInfoAsJson( $retValue );
 	}
 
